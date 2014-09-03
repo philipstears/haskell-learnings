aff4c0b550b87b66188d068204c4b77a6c25af31
Philip Stears <philip@driveworks.co.uk>
2014-09-03 09:55:35 +0100

    Start of message parser (errors)

--- upenn/ex/2/LogAnalysis.hs
+++ upenn/ex/2/LogAnalysis.hs
@@ -4,4 +4,8 @@ module LogAnalysis where
 
 import Log
 
+parseMessage :: String -> LogMessage
 
+parseMessage line = parseMessageCore $ words line
+    where
+        parseMessageCore ("E":severity:timestamp:rest) = LogMessage (Error (read severity :: Int)) (read timestamp :: Int) (unwords rest)
